7d90fad65bab9c4d0af8c88edb2bcf490d69c9c5
Joseph Heywood <beywood88@gmail.com>
2024-07-22 15:14:04 +0200

    Generic functions added to utils.py

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@
 Utility functions for the RGB.
 """
 
-from numpy import delete, r_
+from numpy import delete, r_, array, linalg, column_stack, cos, sin
 def remove_overlap(array, overlap):
     """
     Removes overlap scans from the start and end of the array.
@@ -66,3 +66,39 @@ def deinterleave_bits(z):
         x |= (z & (1 << (2*i))) >> i
         y |= (z & (1 << (2*i + 1))) >> (i + 1)
     return x, y
+
+# function to normalize a vector (or a list of vectors) to 1
+def normalize(vec):
+    vec = array(vec)
+    if len(vec.shape) > 1:
+        vec_norm = array(vec) / linalg.norm(vec, axis=1)[:, None]
+    else:
+        vec_norm = array(vec) / linalg.norm(vec)
+    return vec_norm
+
+def generic_transformation_matrix(x0, y0, z0, x1, y1, z1):
+    A = column_stack((x0, y0, z0))
+    B = column_stack((x1, y1, z1))
+    R = B @ linalg.inv(A)
+    return R
+
+# returns the rotation matrix around a given axis and a given angle
+def rotation_matrix(axis, angle):
+    assert axis in ['x', 'y', 'z']
+
+    if axis == 'x':
+        matrix = array([[1, 0, 0],
+                           [0, cos(angle), -sin(angle)],
+                           [0, sin(angle), cos(angle)]])
+
+    if axis == 'y':
+        matrix = array([[cos(angle), 0, sin(angle)],
+                           [0, 1, 0],
+                           [-sin(angle), 0, cos(angle)]])
+
+    if axis == 'z':
+        matrix = array([[cos(angle), -sin(angle), 0],
+                           [sin(angle), cos(angle), 0],
+                           [0, 0, 1]])
+
+    return matrix
