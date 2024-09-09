592ca9df27a13cd543f7bd9a927ecb4d1ac2b73a
Joseph Heywood <beywood88@gmail.com>
2024-09-09 14:55:24 +0200

    - No changes from original main. - Add a save path at end of script if you don't work in a console. - Intermediate results check at line 45 to open a variable from the data_dict_out and add it to an EASE grid.

--- src/main.py
+++ src/main.py
@@ -8,10 +8,11 @@ It is also responsible for returning the output to the caller
 """
 import os
 import pickle
+from numpy import full, nan
 
 from data_ingestion import DataIngestion
-from grid_generator import GridGenerator
-from regridder import ReGridder
+from grid_generator import GridGenerator, GRIDS
+from regridder_v2 import ReGridder
 
 # ---- Testing ----
 import matplotlib
@@ -30,8 +31,6 @@ if __name__ == '__main__':
     config = ingestion_object.config
     data_dict = ingestion_object.ingest_data()
 
-    # test = ids_kernel_weights(config, data_dict)
-
     # Regrid Data
     if config.input_data_type == 'SMAP':
         data_dict_out = ReGridder(config).regrid_l1c(data_dict)
@@ -40,42 +39,33 @@ if __name__ == '__main__':
         data_dict_out = ReGridder(config).regrid_l1r(data_dict)
 
     if config.input_data_type == 'CIMR':
-        data_dict_out = {}
-        for band in data_dict:
-            data_dict_out[band] = ReGridder(config_object=config, band_to_remap=band).regrid_l1c(data_dict[band])
+            data_dict_out= ReGridder(config).regrid_l1c(data_dict)
+
+
+    # Intermediate results check
+    # Put in the variables you want from the data_dict_out in data_dict.
+    # grid_shape = GRIDS[config.grid_definition]['n_rows'], GRIDS[config.grid_definition]['n_cols']
+    # # create nan array with shape of grid_shape
+    # grid = full(grid_shape, nan)
+    # variable = data_dict_out['bt_h_fore']
+    # cell_row = data_dict_out['cell_row_fore']
+    # cell_col = data_dict_out['cell_col_fore']
+    # for i in range(len(cell_row)):
+    #     grid[cell_row[i], cell_col[i]] = variable[i]
+
+    # Load equivalent L1c Data here if you want to compare
 
 
 
-    if config.save_to_disk == True:
-        if config.grid_type == 'L1C':
-            grid_string = config.grid_definition
-        elif config.grid_type == 'L1R':
-            grid_string = f'{config.source_band}_{config.target_band}'
-        if config.input_data_type == 'SMAP':
-            replace_in = 'L1B'
-            replace_out = config.grid_type
-        elif config.input_data_type == 'AMSR2':
-            replace_in = 'GBT'
-            if config.grid_type == 'L1C':
-                replace_out = 'GCT'
-            elif config.grid_type == 'L1R':
-                replace_out = 'GRT'
-                grid_string = grid_string + f'_{str(config.kernel_size)}'
-        elif config.input_data_type == 'CIMR':
-            replace_in = 'l1b'
-            replace_out = config.grid_type
+    # Temp save solution
+    # save_path = ''
+    # # Save pickle dictionary to save path
+    # with open(save_path, 'wb') as f:
+    #     pickle.dump(data_dict_out, f)
 
-        save_path = os.path.join(
-           config.dpr_path,
-           config.grid_type,
-           config.input_data_type,
-           'RGB',
-           config.input_data_path.split('/')[-1].split('.')[0].replace(replace_in, replace_out) +
-            '_' + grid_string + '.pkl')
-        with open(save_path, 'wb') as f:
-             pickle.dump(data_dict_out, f)
-        print(f"Data saved to:"
-              f"{save_path}")
+
+
+
 
 
 
