caffe2c5821b96bb1956a08bb6a28eca15d43099
Joseph Heywood <beywood88@gmail.com>
2024-10-31 13:59:06 +0100

    - Initiation of Antenna patterns moved to init - renamed file bg_l1r to bg --> deleted old version

--- src/bg.py
+++ src/bg.py
@@ -1,8 +1,6 @@
-from numpy.core.multiarray import unravel_index
-
 from grid_generator import GridGenerator, GRIDS
 from ap_processing import AntennaPattern
-from  numpy import where, nan, take, full, all, sum, zeros, identity, dot, nansum
+from  numpy import where, nan, take, full, all, sum, zeros, identity, dot, nansum, unravel_index, rad2deg
 from numpy.linalg import inv
 from tqdm import tqdm
 
@@ -13,95 +11,106 @@ import matplotlib.pyplot as plt
 
 
 class BGInterp:
-    def __init__(self, config):
+    def __init__(self, config, band):
         self.config = config
+        self.band = band
+        self.source_ap = AntennaPattern(config=self.config,
+                                        band=self.band,
+                                        antenna_method=self.config.source_antenna_method,
+                                        polarisation_method=self.config.polarisation_method,
+                                        antenna_threshold=self.config.source_antenna_threshold,
+                                        gaussian_params=self.config.source_gaussian_params)
+        if self.config.grid_type == 'L1R':
+            self.target_ap = AntennaPattern(config=self.config,
+                                            band=self.config.target_band[0],
+                                            antenna_method=self.config.target_antenna_method,
+                                            polarisation_method=self.config.polarisation_method,
+                                            antenna_threshold=self.config.target_antenna_threshold,
+                                            gaussian_params=self.config.target_gaussian_params)
+        else:
+            target_ap = None
 
-    def get_antenna_patterns(self, band, longitude, latitude, x_pos, y_pos, z_pos, x_vel, y_vel,
-                             z_vel, processing_scan_angle, feed_horn_number, attitude, target_lon, target_lat):
 
-
-        # Initiate Source Pattern
-        source_ap = AntennaPattern(config=self.config,
-                                   band=band)
-
+    def get_antenna_patterns(self, band, variable_dict, target_dict, target_lon, target_lat, source_inds, target_inds, target_cell_size=None):
 
         # Make integration grid
-        int_dom_lons, int_dom_lats = source_ap.make_integration_grid(
-            longitude=longitude,
-            latitude=latitude
+        int_dom_lons, int_dom_lats = self.source_ap.make_integration_grid(
+            longitude=variable_dict['longitude'][source_inds],
+            latitude=variable_dict['latitude'][source_inds]
         )
 
         # Project source patterns to grid
         source_ant_patterns = []
-        for sample in range(longitude.shape[0]):
-            sample_pattern=source_ap.antenna_pattern_to_earth(
+        for sample in source_inds: # CHECK SAMPLE
+            sample_pattern=self.source_ap.antenna_pattern_to_earth(
                 int_dom_lons=int_dom_lons,
                 int_dom_lats=int_dom_lats,
-                x_pos=x_pos[sample],
-                y_pos=y_pos[sample],
-                z_pos=z_pos[sample],
-                x_vel=x_vel[sample],
-                y_vel=y_vel[sample],
-                z_vel=z_vel[sample],
-                processing_scan_angle=processing_scan_angle[sample],
-                feed_horn_number=feed_horn_number[sample],
-                attitude=attitude[sample],
-                lon_l1b = longitude[sample],
-                lat_l1b = latitude[sample]
+                x_pos=variable_dict['x_position'][sample],
+                y_pos=variable_dict['y_position'][sample],
+                z_pos=variable_dict['z_position'][sample],
+                x_vel=variable_dict['x_velocity'][sample],
+                y_vel=variable_dict['y_velocity'][sample],
+                z_vel=variable_dict['z_velocity'][sample],
+                processing_scan_angle=variable_dict['processing_scan_angle'][sample],
+                feed_horn_number=variable_dict['feed_horn_number'][sample],
+                attitude=variable_dict['attitude'][sample],
+                lon_l1b = variable_dict['longitude'][sample],
+                lat_l1b = variable_dict['latitude'][sample]
             )
             sample_pattern /= sum(sample_pattern)
             source_ant_patterns.append(sample_pattern)
 
-        # Project target patterns to grid
+        # Get target patterns
+        target_ant_pattern = None
         if self.config.grid_type == 'L1R':
-            target_ap = AntennaPattern(config=self.config,
-                                       band=self.config.target_band)
 
-            # At this point, we also need the source data.
-
-
-
-        else:
-            target_ant_pattern = source_ap.target_gaussian(int_dom_lons,
-                                                int_dom_lats,
-                                                target_lon,
-                                                target_lat)
+            target_ant_pattern = self.target_ap.antenna_pattern_to_earth(
+                int_dom_lons=int_dom_lons,
+                int_dom_lats=int_dom_lats,
+                x_pos=target_dict['x_position'][target_inds],
+                y_pos=target_dict['y_position'][target_inds],
+                z_pos=target_dict['z_position'][target_inds],
+                x_vel=target_dict['x_velocity'][target_inds],
+                y_vel=target_dict['y_velocity'][target_inds],
+                z_vel=target_dict['z_velocity'][target_inds],
+                processing_scan_angle=target_dict['processing_scan_angle'][target_inds],
+                feed_horn_number=target_dict['feed_horn_number'][target_inds],
+                attitude=target_dict['attitude'][target_inds],
+                lon_l1b=target_lon,
+                lat_l1b=target_lat
+            )
             target_ant_pattern /= sum(target_ant_pattern)
 
-        # Checking the shift factor, there shouldnt be an array where they are all zero
-        for count, pattern in enumerate(source_ant_patterns):
-            if all(pattern == 0):
-                print(f"Antenna pattern not succesfully projected to grid! - DEBUG!")
+        elif self.config.grid_type == 'L1C':
+            sigma_lon, sigma_lat = target_cell_size
+            target_ant_pattern = AntennaPattern.target_gaussian(int_dom_lons, int_dom_lats, target_lon, target_lat, 
+                                                                sigma_lon, sigma_lat, rot=0.)
 
         return source_ant_patterns, target_ant_pattern
 
-    def BG(self, band, samples_dict, target_grid, longitude,
-           latitude, x_pos, y_pos, z_pos, x_vel, y_vel, z_vel, processing_scan_angle,
-           feed_horn_number, attitude):
+    def BG(self, band, samples_dict, variable_dict, target_dict, target_grid):
 
         indexes = samples_dict['indexes']
-        fill_value = len(longitude)
+        fill_value = len(variable_dict['longitude'])
         weights = full((indexes.shape[0], indexes.shape[1]), nan)
 
         for target_cell in tqdm(range(indexes.shape[0])):
 
             # Getting the target lon, lat
             if self.config.grid_type == 'L1C':
-                # I get two different results here for target_lon, target_lat. They should be the same
-                # I suspect that there may be an issue with the conversion functions in the grid generator.
-                # THIS SHOULD BE LOOKED INTO AND VERIFIED
-                # MAYBE THE UNRAVEL INDEX SHOULD ACTUALLY BE DONE USING SCAN AND SAMPLE NUMBERS
-                target_row, target_col = unravel_index(samples_dict['grid_1d_index'][target_cell], (GRIDS[self.config.grid_definition]['n_rows'], GRIDS[self.config.grid_definition]['n_cols']))
-                target_lon, target_lat = GridGenerator(self.config).rowcol_to_lonlat(target_row, target_col)
+                target_lon, target_lat = (target_grid[0].flatten('C')[samples_dict['grid_1d_index'][target_cell]],
+                                          target_grid[1].flatten('C')[samples_dict['grid_1d_index'][target_cell]])
 
-                # target_lon_test, target_lat_test = (target_grid[0].flatten('C')[samples_dict['grid_1d_index'][target_cell]],
-                #                                     target_grid[1].flatten('C')[samples_dict['grid_1d_index'][target_cell]])
+                # TODO: this works only with EASE 9km grid.. for other grids, the size of the cell should be estimated in grid_generator
+                Rearth  = (6378137. + 6356752.)/2. #m
+                resolution = GridGenerator(self.config, self.config.projection_definition, self.config.grid_definition).resolution
+                angle   = rad2deg(resolution/Rearth)
+                target_cell_size = [angle, angle]
 
             elif self.config.grid_type == 'L1R':
-                # Scan Geometry, is it still unravel index? I think so
+                target_cell_size = None
                 target_lon, target_lat = (target_grid[0][samples_dict['grid_1d_index'][target_cell]],
                                           target_grid[1][samples_dict['grid_1d_index'][target_cell]])
-
 
             # Get Antenna Patterns
             samples = indexes[target_cell, :]
@@ -109,19 +118,13 @@ class BGInterp:
             # print(f"input_samples: {input_samples}")
             source_ant_patterns, target_ant_pattern = self.get_antenna_patterns(
                 band=band,
-                longitude=longitude[input_samples],
-                latitude=latitude[input_samples],
-                x_pos=x_pos[input_samples],
-                y_pos=y_pos[input_samples],
-                z_pos=z_pos[input_samples],
-                x_vel=x_vel[input_samples],
-                y_vel=y_vel[input_samples],
-                z_vel=z_vel[input_samples],
-                processing_scan_angle=processing_scan_angle[input_samples],
-                feed_horn_number=feed_horn_number[input_samples],
-                attitude=attitude[input_samples],
+                variable_dict=variable_dict,
+                target_dict=target_dict,
                 target_lon = target_lon,
-                target_lat = target_lat
+                target_lat = target_lat,
+                source_inds=input_samples,
+                target_inds=samples_dict['grid_1d_index'][target_cell], 
+                target_cell_size=target_cell_size
             )
 
             # BG algorithm
@@ -146,49 +149,57 @@ class BGInterp:
 
         return weights
 
-    def get_weights(self, band, samples_dict, variable_dict, target_grid, scan_direction):
+    def get_weights(self, band, samples_dict, variable_dict, target_dict, target_grid, scan_direction):
 
+        # Preparing variable_dict
         if scan_direction:
             scan_direction = f"_{scan_direction}"
         else:
             scan_direction = ""
 
-        if f"attitude{scan_direction}" in variable_dict:
-            attitude = variable_dict[f"attitude{scan_direction}"]
-        else:
-            attitude = full(variable_dict[f"longitude{scan_direction}"].shape, None)
+        # Adding a empty attitude variable for SMAP
+        if f"attitude{scan_direction}" not in variable_dict:
+            variable_dict[f'attitude{scan_direction}'] = full(variable_dict[f"longitude{scan_direction}"].shape, None)
+
+        variable_dict={key.removesuffix(f'{scan_direction}'): value for key, value in variable_dict.items()}
 
         weights = self.BG(
             band=band,
             samples_dict=samples_dict,
+            variable_dict=variable_dict,
+            target_dict=target_dict,
             target_grid=target_grid,
-            longitude=variable_dict[f"longitude{scan_direction}"],
-            latitude=variable_dict[f"latitude{scan_direction}"],
-            x_pos=variable_dict[f'x_position{scan_direction}'],
-            y_pos=variable_dict[f'y_position{scan_direction}'],
-            z_pos=variable_dict[f'z_position{scan_direction}'],
-            x_vel=variable_dict[f'x_velocity{scan_direction}'],
-            y_vel=variable_dict[f'y_velocity{scan_direction}'],
-            z_vel=variable_dict[f'z_velocity{scan_direction}'],
-            processing_scan_angle=variable_dict[f'processing_scan_angle{scan_direction}'],
-            feed_horn_number=variable_dict[f'feed_horn_number{scan_direction}'],
-            attitude=attitude
         )
 
         return weights
 
-    def interp_variable_dict(self, samples_dict, variable_dict, target_grid, scan_direction=None, band=None, **args):
+    def interp_variable_dict(self, **kwargs):
 
-        # This gets opened twice on split fore/aft, maybe changes this to be more efficient. could add to config
-        # ap = AntennaPattern(
-        #     config=self.config,
-        #     band=band
-        # )
+
+        if self.config.grid_type == 'L1C':
+            samples_dict = kwargs['samples_dict']
+            variable_dict = kwargs['variable_dict']
+            target_dict = None
+            target_grid = kwargs['target_grid']
+            scan_direction = kwargs['scan_direction']
+            band = kwargs['band']
+
+        elif self.config.grid_type == 'L1R':
+            samples_dict = kwargs['samples_dict']
+            variable_dict = kwargs['variable_dict']
+            target_dict = kwargs['target_dict']
+            target_grid = kwargs['target_grid']
+            scan_direction = kwargs['scan_direction']
+            band = kwargs['band']
+
+        if scan_direction is not None:
+            variable_dict = {key: value for key, value in variable_dict.items() if key.endswith(scan_direction)}
 
         weights = self.get_weights(
             band=band,
             samples_dict=samples_dict,
             variable_dict=variable_dict,
+            target_dict=target_dict,
             target_grid=target_grid,
             scan_direction=scan_direction
         )
