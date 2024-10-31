996ffe41c813208c78078ba8f854b5d0f8b61177
Joseph Heywood <beywood88@gmail.com>
2024-10-31 16:29:44 +0100

    - Update before push to devel

--- src/main.py
+++ src/main.py
@@ -31,26 +31,27 @@ if __name__ == '__main__':
 
     # Regrid Data
     if config.input_data_type == 'SMAP':
-        data_dict_out = ReGridder(config).regrid_l1c(data_dict)
+            data_dict_out = ReGridder(config).regrid_l1c(data_dict)
 
     if config.input_data_type == 'AMSR2':
-        data_dict_out = ReGridder(config).regrid_l1c(data_dict)
+            data_dict_out = ReGridder(config).regrid_l1c(data_dict)
 
     if config.input_data_type == 'CIMR':
-            data_dict_out= ReGridder(config).regrid_l1c(data_dict)
+            data_dict_out = ReGridder(config).regrid_l1c(data_dict)
 
 
     # Intermediate results check
     # Put in the variables you want from the data_dict_out in data_dict.
-    # grid_shape = GRIDS[config.grid_definition]['n_rows'], GRIDS[config.grid_definition]['n_cols']
-    # # # create nan array with shape of grid_shape
-    # grid = full(grid_shape, nan)
-    # variable = data_dict_out['L']['bt_h_fore']
-    # cell_row = data_dict_out['L']['cell_row_fore']
-    # cell_col = data_dict_out['L']['cell_col_fore']
-    # for i in range(len(cell_row)):
-    #     grid[cell_row[i], cell_col[i]] = variable[i]
-    # plt.imshow(grid)
+    if config.grid_type == 'L1C':
+        grid_shape = GRIDS[config.grid_definition]['n_rows'], GRIDS[config.grid_definition]['n_cols']
+        # # create nan array with shape of grid_shape
+        grid = full(grid_shape, nan)
+        variable = data_dict_out['C']['bt_h']
+        cell_row = data_dict_out['C']['cell_row']
+        cell_col = data_dict_out['C']['cell_col']
+        for i in range(len(cell_row)):
+            grid[cell_row[i], cell_col[i]] = variable[i]
+        plt.imshow(grid)
     # Load equivalent L1c Data here if you want to compare
 
     # Intermediate results check
@@ -74,15 +75,17 @@ if __name__ == '__main__':
     #     pickle.dump(data_dict_out, f)
 
     # If the prodct is L1r we build the array as follows
-    # if config.grid_type == 'L1R':
-    #     # Build an array of nans
-    #     variable = 'bt_h_fore'
-    #     grid_shape = config.num_target_scans, config.num_target_samples
-    #     grid = full(grid_shape, nan)
-    #     scan_number = data_dict_out['C']['cell_row_fore']
-    #     sample_number = data_dict_out['C']['cell_col_fore']
-    #     for count, sample in enumerate(data_dict_out['C'][variable]):
-    #         grid[int(scan_number[count]), int(sample_number[count])] = sample
+    if config.grid_type == 'L1R':
+        # Build an array of nans
+        variable = 'bt_h'
+        grid_shape = config.num_target_scans, config.num_target_samples
+        grid = full(grid_shape, nan)
+        scan_number = data_dict_out['C']['cell_row']
+        sample_number = data_dict_out['C']['cell_col']
+        for count, sample in enumerate(data_dict_out['C'][variable]):
+            grid[int(scan_number[count]), int(sample_number[count])] = sample
+        plt.imshow(grid)
+
     #     test = 0
 
 
