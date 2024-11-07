ee33a0dc731384999e9e3b444aba6893c4b100c5
Joseph Heywood <beywood88@gmail.com>
2024-11-07 12:10:01 +0100

    - atan2 --> arctan2

--- src/ap_processing.py
+++ src/ap_processing.py
@@ -522,7 +522,7 @@ def haversine_distance(lon1, lat1, lon2, lat2):
     dlat = lat2_rad - lat1_rad
     dlon = lon2_rad - lon1_rad
     a = np.sin(dlat / 2)**2 + np.cos(lat1_rad) * np.cos(lat2_rad) * np.sin(dlon / 2)**2
-    c = 2 * np.atan2(np.sqrt(a), np.sqrt(1 - a))
+    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
     distance = Rearth * c
 
     return distance #m
