02836b0344d3b9d69ddbb92be2dc48ef10a99066
Joseph Heywood <beywood88@gmail.com>
2024-12-11 23:21:45 +0100

    - Updating tests

--- tests/MS3_verification_tests/T_21/T_21.py
+++ tests/MS3_verification_tests/T_21/T_21.py
@@ -18,14 +18,12 @@ from grid_generator import GRIDS
 from numpy import array, full, nan, nanmean, polyfit, isnan, isinf
 
 # Add RGB remapped IDS netCDF here
-ids_data_path = '/home/beywood/ST/CIMR_RGB/CIMR-RGB/tests/MS3_verification_tests/T_21/CIMR_L1R_IDS_2024-12-08_19-01-38.nc'
-# ids_data_path = '/home/beywood/ST/CIMR_RGB/CIMR-RGB/tests/MS3_verification_tests/CIMR_L1R_IDS_36km_2024-12-04_13-06-26_SR30.nc'
-# ids_data_path = '/home/beywood/ST/CIMR_RGB/CIMR-RGB/tests/MS3_verification_tests/CIMR_L1R_IDS_36km_2024-12-04_13-09-31_SR100.nc'
-# ids_data_path = '/home/beywood/ST/CIMR_RGB/CIMR-RGB/tests/MS3_verification_tests/CIMR_L1R_IDS_36km_2024-12-04_13-10-09_SR200.nc'
+ids_data_path = ''
+
 # Add the RGB remapped RSIR netCDF here
 
 
-rsir_data_path = '/home/beywood/ST/CIMR_RGB/CIMR-RGB/tests/MS3_verification_tests/T_21/CIMR_L1R_RSIR_2024-12-08_18-58-35.nc'
+rsir_data_path = ''
 
 BAND = 'X_BAND'
 PROJECTION = 'C_BAND_TARGET'
