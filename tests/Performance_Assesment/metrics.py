41681e14ecffd43f1fda37ccd14637ac9d245533
Joseph Heywood <beywood88@gmail.com>
2025-02-13 11:43:20 +0100

    - Adding first version of parameter optimisation scripts - Adding example of results

---
+++ tests/Performance_Assesment/metrics.py
@@ -0,0 +1,107 @@
+import numpy as np
+import cv2
+
+import matplotlib
+tkagg = matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+plt.ion()
+
+
+def _validate_arrays(X, Xref, check_2d=False):
+    if X.shape != Xref.shape:
+        raise ValueError(f"Images should have the same shape, now {X.shape} and {Xref.shape}")
+    if not np.any(X):
+        raise ValueError("Regridded image is empty")
+    if not np.any(Xref):
+        raise ValueError("Reference image is empty")
+    if check_2d:
+        if len(X.shape) != 2:
+            raise ValueError("Regridded image should be passed as 2d array for this metric")
+        if len(Xref.shape) != 2:
+            raise ValueError("Reference image should be passed as 2d array for this metric")
+
+
+def normalised_difference(X, Xref):
+    # Davide, I have renames this function from "relative_root_mean_square_error" to
+    # "normalised_distance" as I think the original name is incorrect. I got the current name
+    # from the LW paper. (Delete comment if you agree)
+
+    _validate_arrays(X, Xref)
+    valid_mask = ~np.isnan(Xref) & ~np.isnan(X)
+    return np.linalg.norm(X[valid_mask]-Xref[valid_mask]) / np.linalg.norm(Xref[valid_mask])
+
+def root_mean_square_error(X, Xref):
+    _validate_arrays(X, Xref)
+    valid_mask = ~np.isnan(Xref) & ~np.isnan(X)
+
+    # Mean of squared differences
+    mse = np.mean((X[valid_mask]-Xref[valid_mask])**2)
+
+    return np.sqrt(mse)
+
+def standard_deviation_error(X, Xref):
+    _validate_arrays(X, Xref)
+    valid_mask = ~np.isnan(Xref) & ~np.isnan(X)
+
+    diff = X[valid_mask]-Xref[valid_mask]
+
+    return np.std(diff)
+
+def pointwise_correlation(X, Xref):
+    _validate_arrays(X, Xref)
+    valid_mask = ~np.isnan(Xref) & ~np.isnan(X)
+    return np.corrcoef(X[valid_mask].flatten(), Xref[valid_mask].flatten())[0, 1]
+
+
+
+
+
+def relative_global_error(X, Xref):
+    _validate_arrays(X, Xref)
+    return np.linalg.norm((X-Xref)/X)
+
+
+def total_relative_absolute_error(X, Xref):
+    _validate_arrays(X, Xref)
+    return np.sum(np.abs((X-Xref)/X))
+
+
+def mean_absolute_error(X, Xref):
+    _validate_arrays(X, Xref)
+    valid_mask = ~np.isnan(Xref) & ~np.isnan(X)
+    return np.mean(np.abs(X[valid_mask]-Xref[valid_mask]))
+
+
+def improvement_factor(X, Xref, threshold_dB=-3):
+    print("Note: The improvement factor should be calculated only on spot images")
+    _validate_arrays(X, Xref)
+    Xmax = X.max()
+    Xrefmax = Xref.max()
+    return np.sqrt(X[10*np.log10(X/Xmax)>=threshold_dB].size / Xref[10*np.log10(Xref/Xrefmax)>=threshold_dB].size)
+
+
+def peak_error(X, Xref):
+    print("Note: The peak error should be calculated only on spot images")
+    _validate_arrays(X, Xref)
+    return Xref.max() - X.max()
+
+
+def sharpening_factor(X, Xref):
+    import matplotlib.pyplot as plt
+
+    _validate_arrays(X, Xref, check_2d=True)
+
+    def gradient_magnitude(image):
+        grad_x = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=3)
+        grad_y = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=3)
+        magnitude = np.sqrt(grad_x**2 + grad_y**2)
+        return np.sum(magnitude)
+
+    Xgrad    = gradient_magnitude(X)
+    Xrefgrad = gradient_magnitude(Xref)
+
+    if not np.any(Xrefgrad):
+        raise ValueError("Reference image has no gradient, i.e. is a constant image")
+
+    return Xgrad / Xrefgrad
+    
